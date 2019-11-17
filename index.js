4cf51b1be93a1ed805bfe97dc9d2d7d8e4c51e54
liudicong <liudicong@xunlei.com>
2019-11-17 13:11:24 +0800

    fix

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ if (newPath.length > 0) {
     }
     console.log('newPath:'+newPath)
     console.log('正在跳转...')
-    var newHref = window.location.href.replace('/index.html', newPath)
-    console.log(newHref)
-    window.location.href = newHref
+    window.location.href = window.location.href.replace('/index.html', newPath)
+    console.log(window.location.href)
 }
