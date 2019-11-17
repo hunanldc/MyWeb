97de4475e1def9558ca3b98803e40584bb166fc1
liudicong <liudicong@xunlei.com>
2019-11-17 12:50:12 +0800

    添加调试

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ var newPath = '/demo/index.html'
 if (newPath.startsWith('/') === false) {
     newPath = '/'+newPath
 }
-window.location.href = window.location.href.replace('/index.html', newPath)
+console.log('正在跳转...')
+var newHref = window.location.href.replace('/index.html', newPath)
+console.log(newHref)
+window.location.href = newHref
