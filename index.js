7c48841dc9acb3456dfa054733a360b8f13d8588
liudicong <liudicong@xunlei.com>
2019-11-17 13:05:17 +0800

    fix

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var newPath = '/demo/index.html'
-
+console.log('newPath:'+newPath)
 if (newPath.length == 0) {
     return
 } else if (newPath.startsWith('/') === false) {
     newPath = '/'+newPath
 }
+console.log('newPath:'+newPath)
 console.log('正在跳转...')
 var newHref = window.location.href.replace('/index.html', newPath)
 console.log(newHref)
