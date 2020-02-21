8df09343706469ea7f4d3c3d0a8b06b9e566897f
liudicong <liudicong@xunlei.com>
2020-02-22 03:00:37 +0800

    bug fix

--- jinglian/js/index.js
+++ jinglian/js/index.js
@@ -38,97 +38,8 @@ new Vue({
                 diSha:0.035//失败掉1级
             },
             hiddenDetail: false
-        }
-    },
-    computed: {
-        suggestions: function () {
-            let resultArray = []
-            let huanXianPerTime = 1
-            let min = function(a, b, c) {
-                a=a()
-                b=b()
-                c=c()
-                let minab = a.cost < b.cost ? a : b
-                return minab.cost < c.cost ? minab : c
-            }
-            for (i=0; i<=11; i++) {
-                let preLevelObj =  [...resultArray].pop() || {
-                    cost: 0,
-                    huanXian: 0,
-                    tianGang: 0,
-                    diSha: 0,
-                    rate: 0
-                }
-                let preLevelPrice = preLevelObj.cost
-                let price = {
-                    huanXianOnly: ()=> {
-                        let rate = this.config.baseRate[i]
-                        let preLevelCost = preLevelPrice
-                        let currentCost = huanXianPerTime * this.config.price.huanXian
-                        let costPerTime = preLevelCost + currentCost
-                        console.log('精炼',i,'升',i+1,' 辅助：幻仙',' 成功率', rate,'，单次成本',preLevelCost,'+',currentCost,'合计',costPerTime,' 成功成本',costPerTime / rate)
-                        return {
-                            cost: costPerTime / rate,
-                            name: '幻仙',
-                            level: i,
-                            huanXian: (preLevelObj.huanXian + 1) / rate,
-                            tianGang: preLevelObj.tianGang / rate,
-                            diSha: preLevelObj.diSha / rate,
-                            rate: rate
-                        }
-                    },
-                    addTianGang: ()=> {
-                        let rate = this.config.baseRate[i] + this.config.fuzhushiRate.tianGang
-                        let preLevelCost = preLevelPrice
-                        let currentCost = huanXianPerTime * this.config.price.huanXian + this.config.price.tianGang
-                        let costPerTime = preLevelCost + currentCost
-                        console.log('精炼',i,'升',i+1,' 辅助：天罡',' 成功率', rate,'，单次成本',preLevelCost,'+',currentCost,'合计',costPerTime,' 成功成本',costPerTime / rate)
-                        return {
-                            cost: costPerTime / rate,
-                            name: '天罡',
-                            level: i,
-                            huanXian: (preLevelObj.huanXian + 1) / rate,
-                            tianGang: (preLevelObj.tianGang + 1) / rate,
-                            diSha: preLevelObj.diSha / rate,
-                            rate: rate
-                        }
-                    },
-                    addDiSha: ()=> {
-                        let rate = this.config.baseRate[i] + this.config.fuzhushiRate.diSha
-                        let preLevelCost = preLevelPrice
-                        let currentCost = huanXianPerTime * this.config.price.huanXian + this.config.price.diSha
-                        //地煞石，精炼失败，只会降1级，还有剩余价值
-                        let left = {
-                            cost: 0,
-                            huanXian: 0,
-                            tianGang: 0,
-                            diSha: 0
-                        }
-                        let leftNum = (1/rate - 1)
-                        if (resultArray.length > 1) {
-                            let prePreObj = resultArray[resultArray.length - 2]
-                            left.cost = prePreObj.cost * (1/rate - 1)
-                            left.huanXian = prePreObj.huanXian * (1/rate - 1)
-                            left.tianGang = prePreObj.tianGang * (1/rate - 1)
-                            left.diSha = prePreObj.diSha * (1/rate - 1)
-                        }
-                        let costPerTime = preLevelCost + currentCost
-                        console.log('精炼',i,'升',i+1,' 辅助：地煞',' 成功率', rate,'，单次成本',preLevelCost,'+',currentCost,'合计',costPerTime,' 成功成本',costPerTime / rate - left.cost)
-                        return {
-                            cost: costPerTime / rate - left.cost,
-                            name: '地煞',
-                            level: i,
-                            huanXian: (preLevelObj.huanXian + 1) / rate - left.huanXian,
-                            tianGang: (preLevelObj.tianGang) / rate - left.tianGang,
-                            diSha: (preLevelObj.diSha + 1) / rate - left.diSha,
-                            rate: rate
-                        }
-                    }
-                }
-                resultArray.push(min(price.huanXianOnly, price.addTianGang, price.addDiSha))
-            }
-            return resultArray
-        }
+        },
+        suggestions:[]
     },
     watch: {
         config: {
@@ -138,7 +49,118 @@ new Vue({
             }
         }
     },
+    mounted: function () {
+        debugger
+        this.readData()
+        this.updateSuggestions()
+    },
     methods: {
+        updateSuggestions() {
+            let resultArray = []
+            let huanXianPerTime = 1
+            let min = function(a, b, c) {
+                // a=a()
+                // b=b()
+                // c=c()
+                let minab = a.cost < b.cost ? a : b
+                let ret = minab.cost < c.cost ? minab : c
+                console.log(a.level, a, b, c, ret)
+                return ret
+            }
+            const configPrice = {
+                huanXian:parseInt(this.config.price.huanXian),
+                tianGang:parseInt(this.config.price.tianGang),
+                diSha:parseInt(this.config.price.diSha)
+            }
+            for (i=0; i<=11; i++) {
+                let preLevelObj =  [...resultArray].pop() || {
+                    cost: 0,
+                    huanXian: 0,
+                    tianGang: 0,
+                    diSha: 0,
+                    rate: 0
+                }
+                if (i===3) {
+                    debugger
+                }
+                let preLevelPrice = preLevelObj.cost
+                const price = {}
+                {
+                    let rate = this.config.baseRate[i]
+                    let preLevelCost = preLevelPrice
+                    let currentCost = huanXianPerTime * configPrice.huanXian
+                    let costPerTime = preLevelCost + currentCost
+                    // console.log('精炼',i,'升',i+1,' 辅助：幻仙',' 成功率', rate,'，单次成本',preLevelCost,'+',currentCost,'合计',costPerTime,' 成功成本',costPerTime / rate)
+                    price.huanXianOnly = {
+                        cost: costPerTime / rate,
+                        name: '幻仙',
+                        level: i,
+                        huanXian: (preLevelObj.huanXian + 1) / rate,
+                        tianGang: preLevelObj.tianGang / rate,
+                        diSha: preLevelObj.diSha / rate,
+                        rate: rate
+                    }
+                }
+                {
+                    let rate = this.config.baseRate[i] + this.config.fuzhushiRate.tianGang
+                    let preLevelCost = preLevelPrice
+                    let currentCost = huanXianPerTime * configPrice.huanXian + configPrice.tianGang
+                    let costPerTime = preLevelCost + currentCost
+                    // console.log('精炼',i,'升',i+1,' 辅助：天罡',' 成功率', rate,'，单次成本',preLevelCost,'+',currentCost,'合计',costPerTime,' 成功成本',costPerTime / rate)
+
+                    price.addTianGang = {
+                        cost: costPerTime / rate,
+                        name: '天罡',
+                        level: i,
+                        huanXian: (preLevelObj.huanXian + 1) / rate,
+                        tianGang: (preLevelObj.tianGang + 1) / rate,
+                        diSha: preLevelObj.diSha / rate,
+                        rate: rate
+                    }
+                }
+                {
+                    let rate = this.config.baseRate[i] + this.config.fuzhushiRate.diSha
+                    let preLevelCost = preLevelPrice
+                    let currentCost = huanXianPerTime * configPrice.huanXian + configPrice.diSha
+                    //地煞石，精炼失败，只会降1级，还有剩余价值
+                    let left = {
+                        cost: 0,
+                        huanXian: 0,
+                        tianGang: 0,
+                        diSha: 0
+                    }
+                    let leftNum = (1/rate - 1)
+                    if (resultArray.length > 1) {
+                        let prePreObj = resultArray[resultArray.length - 2]
+                        left.cost = prePreObj.cost * (1/rate - 1)
+                        left.huanXian = prePreObj.huanXian * (1/rate - 1)
+                        left.tianGang = prePreObj.tianGang * (1/rate - 1)
+                        left.diSha = prePreObj.diSha * (1/rate - 1)
+                    }
+                    // console.log('精炼',i,'升',i+1,' 辅助：地煞',' 剩余价值：',left.cost)
+                    let costPerTime = preLevelCost + currentCost
+                    // console.log('精炼',i,'升',i+1,' 辅助：地煞',' 成功率', rate,'，单次成本',preLevelCost,'+',currentCost,'合计',costPerTime,' 成功成本',costPerTime / rate - left.cost)
+                    price.addDiSha = {
+                        cost: costPerTime / rate - left.cost,
+                        name: '地煞',
+                        level: i,
+                        huanXian: (preLevelObj.huanXian + 1) / rate - left.huanXian,
+                        tianGang: (preLevelObj.tianGang) / rate - left.tianGang,
+                        diSha: (preLevelObj.diSha + 1) / rate - left.diSha,
+                        rate: rate
+                    }
+                }
+                let minObj = min(price.huanXianOnly, price.addTianGang, price.addDiSha)
+                resultArray.push(minObj)
+            }
+            console.log('+++++++++++++++++++++++++')
+            console.log(resultArray)
+            console.log('+++++++++++++++++++++++++')
+            resultArray.forEach((item)=> {
+                console.log(`精炼：${item.level}上${item.level + 1}，用：${item.name}，成本：${item.cost/this.config.gameCoinPerYuan}，概率：${item.rate}`)
+            })
+            this.suggestions = resultArray
+        },
         round(val) {
             return Number(val.toString().match(/^\d+(?:\.\d{0,2})?/));
         },
@@ -150,9 +172,6 @@ new Vue({
                 this.config = JSON.parse(localStorage.getItem('config'))
             }
         }
-    },
-    mounted() {
-        this.readData()
     },
     created() {
         // window.alert('hellooworld')
